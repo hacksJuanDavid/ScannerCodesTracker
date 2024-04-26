171cd6b8f1dbd4e100af463586c0a3fb53a723b7
hacksJuanDavid <josuacoty000@gmail.com>
2024-04-25 21:29:47 -0500

    Base code for scanner codes tracker

---
+++ src/pages/Home.tsx
@@ -0,0 +1,28 @@
+import { IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { scanOutline } from 'ionicons/icons';
+import ScannerCodes from '../components/ScannerCodes';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>
+             Scanner Codes Tracker
+            <IonIcon icon={scanOutline} color='primary'/>
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Scanner Codes Tracker</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ScannerCodes />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
