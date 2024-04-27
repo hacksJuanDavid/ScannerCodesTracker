ff49bf360c4f8a46694493069136a91bd0ceb5ea
hacksJuanDavid <josuacoty000@gmail.com>
2024-04-26 19:07:19 -0500

    Add component rewards for user

---
+++ src/components/rewards/rewardForUser.tsx
@@ -0,0 +1,19 @@
+import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from "@ionic/react";
+import RewardBigMacCombo from "../../../public/images/rewardBigMacCombo.png"
+
+// Component rewarForUser
+export default function RewarForUser() {
+    return (
+        <div>
+            <IonCard>
+                <img alt="Silhouette of mountains" src={RewardBigMacCombo} />
+                <IonCardHeader>
+                    <IonCardTitle>Reward for you!</IonCardTitle>
+                    <IonCardSubtitle>You are win a cupon for go to dinner in nigh!!!</IonCardSubtitle>
+                </IonCardHeader>
+
+                <IonCardContent>Is you momment for shared with you friends a best food in Mc donalds</IonCardContent>
+            </IonCard>
+        </div>
+    );
+}
